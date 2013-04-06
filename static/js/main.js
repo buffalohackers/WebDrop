7d8ba6a146eaa9a1e7cfa9c0ef1b7ca366abc0cb
Nate Burgers <nateburgers@gmail.com>
2013-04-06 11:47:49 -0400

    updated file uploader to use a file input

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,26 @@
 function MainController($scope){
     $scope.files = [];
 
-    $scope.drop = function(e){
+    $scope.overwriteEvent = function(e){
 	e.originalEvent.stopPropagation();
 	e.originalEvent.preventDefault();
-	e.originalEvent.dataTransfer.dropEffect = 'copy';
-	var files = e.originalEvent.dataTransfer.files;
+	e.preventDefault();
+	e.stopPropagation();
+    }
+
+    $scope.sendFile = function(file){
+	if(typeof(sendFile) !== 'undefined') sendFile(file);
+    }
+
+    $scope.sendFiles = function(files){
 	if(files.length > 0){
 	    for(var i=0; i<files.length; i++){
 		var file = files[i];
 		var reader = new FileReader();
 		reader.onload = function(e){
-		    $scope.files.push({"name":file.name, "type":file.type, "contents":e.target.result})
+		    var fileData = {"name":file.name, "type":file.type, "contents":e.target.result}
+		    $scope.files.push(fileData);
+		    $scope.sendFile(fileData);
 		    $scope.$apply();
 		}
 		reader.readAsText(file);
@@ -19,5 +28,16 @@ function MainController($scope){
 	};
     }
 
-    $('.drag-box').bind('drop', $scope.drop)
+    $scope.upload = function(e){
+	$scope.overwriteEvent(e);
+	$scope.sendFiles(e.target.files)
+    }
+
+    $scope.drop = function(e){
+	$scope.overwriteEvent(e);
+	$scope.sendFiles(e.originalEvent.dataTransfer.files);
+    }
+    
+    $('.file-upload').bind('change', $scope.upload)
+    $('.file-drop').bind('drop', $scope.drop)
 }
