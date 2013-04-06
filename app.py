8ccedcda77f3dc183c0c76976060bfa1a6d64cd0
jpeacockinc <jpeacockinc@Gmail.com>
2013-04-06 11:31:19 -0400

    flask is running

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+import flask
+from flask import Flask, g
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/")
+def hello():
+	return flask.render_template("index.html")
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 1337))
+    app.run(host='0.0.0.0', port=port)
