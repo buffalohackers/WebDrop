5d0e93aabf86639bf128e3cdc1401fd0949d8ed3
Patrick Jameson <patrickejameson@gmail.com>
2013-04-06 14:03:01 -0400

    maybe

--- web.js
+++ web.js
@@ -1,12 +1,2 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var PeerServer = require('peer').PeerServer;
+var server = new PeerServer({ port: 9000 });
