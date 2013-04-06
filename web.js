3b3197df54fee09a3aad5ce49d8590ad939607a7
Patrick Jameson <patrickejameson@gmail.com>
2013-04-06 15:05:33 -0400

    trying heroku

--- web.js
+++ web.js
@@ -1,6 +1,14 @@
+var fs = require('fs');
 var PeerServer = require('peer').PeerServer;
 var server = new PeerServer({ port: 9000 });
 
-server.get("/", function (request, response) {
-  response.simpleText(200, "Hello World!");
+server._app.get("/", function (request, response) {
+	fs.readFile('./client.html', function (err, html) {
+		if (err) {
+		    throw err; 
+		}
+		response.writeHeader(200, {"Content-Type": "text/html"});
+		response.write(html);
+		response.end();
+	});
 });
