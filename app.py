e95d5802489900d197393585a8eb64c7875a1a01
jpeacockinc <jpeacockinc@Gmail.com>
2013-04-06 15:33:19 -0400

    updated flask

--- app.py
+++ app.py
@@ -5,19 +5,21 @@ from flask import Flask, g
 app = Flask(__name__)
 app.debug = True
 
+
 @app.route("/")
 def hello():
-	return flask.render_template("upload.html")
-@app.route("/index")
+	return flask.render_template("splash.html")
+
+
+@app.route("/upload")
 def index():
-	return flask.render_template("base.html")
+	return flask.render_template("upload.html")
+
+
 @app.route("/receive")
 def receive():
 	return flask.render_template("receive.html")
 
-
-
-	#return flask.render_template("splash.html")
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 1337))
