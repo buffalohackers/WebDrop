fd4fc2291e420873ca4438d5c63958c4bb0bfb21
Patrick Jameson <patrickejameson@gmail.com>
2013-04-06 14:25:33 -0400

    hello world

--- web.js
+++ web.js
@@ -1,2 +1,6 @@
 var PeerServer = require('peer').PeerServer;
 var server = new PeerServer({ port: 9000 });
+
+server.get("/", function (request, response) {
+  response.simpleText(200, "Hello World!");
+});
