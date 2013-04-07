974d95acfb7c4394dfe922d47537ec762b90a0b7
jpeacockinc <jpeacockinc@Gmail.com>
2013-04-06 20:46:09 -0400

    jade and node server

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-//var server = require("./server");
-//var router = require("./route");
-//server.start(router.route);
-var jade = require('jade');
+//create an app server
+var express = require('express')
+app = express();
+//set path to the views (template) directory
+app.set('views', __dirname + '/views');
+//set path to static files
+app.use(express.static(__dirname + '/../public'));
+app.use("/static/css", express.static(__dirname + '/static/css'))
+app.use("/static/js", express.static(__dirname + '/static/js'))
+app.use("/static/img", express.static(__dirname + '/static/img'))
+app.use("/static/font", express.static(__dirname + '/static/font'))
+//handle GET requests on /
+app.get('/', function(req, res){res.render('layoutz.jade', {title: 'test'});});
+app.get('/upload', function(req, res){res.render('uploadLayout.jade', {title: 'test'});});
+app.get('/url', function(req, res){res.render('urlLayout.jade', {title: 'test'});});
+
+//listen on localhost:3000
+app.listen(3000);
